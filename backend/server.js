cc32f9356a97404fa7aa1a667da8955564bf8445
UditBhatt12 <uditbhatt1205@gmail.com>
2026-06-09 10:22:38 +0530

    fix:add vercel url in cors

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,10 @@ io.on('connection', (socket) => {
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://event-media-platform-one.vercel.app'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
